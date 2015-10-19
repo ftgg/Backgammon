ab7381da9fa3ef74471e64dbab0d205c3ee84208
thgnaedi <thomas.gnaedig@htwg-konstanz.de>
2015-10-19 14:10:26 +0200

    Wuerfel implementiert und getestet

--- src/de/htwg/backgammon/Wuerfel.java
+++ src/de/htwg/backgammon/Wuerfel.java
@@ -2,13 +2,33 @@ package de.htwg.backgammon;
 
 public class Wuerfel {
 
-	public Wuerfel(){
-		
+	private int max;
+	private int min;
+
+	public Wuerfel() {
+		this(1,6);
+	}
+
+	public Wuerfel(int min, int max) {
+		this.max = max;
+		this.min = min;
+	}
+
+	public int[] wuerfeln() {
+		int z1, z2;
+		z1 = (int) (Math.random() * max) + min;
+		z2 = (int) (Math.random() * max) + min;
+		return new int[] { z1, z2 };
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public int getMin() {
+		return min;
 	}
 	
-	public int[] wuerfeln() {
-		//TODO
-		return new int[] { 0, 0 };
-	}
+	
 
 }
--- test/de/htwg/backgammon/WuerfelTest.java
+++ test/de/htwg/backgammon/WuerfelTest.java
@@ -2,23 +2,41 @@ package de.htwg.backgammon;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
-public class WuerfelTest extends TestCase{
+public class WuerfelTest extends TestCase {
+	Wuerfel w;
+	
+	@Before
+	public void setUp()throws Exception{
+		w = new Wuerfel();
+	}
 
 	@Test
 	public void testWuerfeln() {
-		Wuerfel w = new Wuerfel();
-		//zahl zwischen [1;6]
+
+		// zahl zwischen [1;6]
 		int[] a = w.wuerfeln();
 		assertNotNull(a);
-		
-		assertTrue(a[0]<7);
-		assertTrue(a[0]>0);
-		assertTrue(a[1]<7);
-		assertTrue(a[1]>0);
+
+		assertTrue(a[0] < 7);
+		assertTrue(a[0] > 0);
+		assertTrue(a[1] < 7);
+		assertTrue(a[1] > 0);
 	}
 
+	 @Test
+	 public void testMin() {
+	 assertNotNull(w.getMin());
+	 assertTrue(w.getMin()>0 && w.getMax() > w.getMin());
+	 }
+	 
+	 @Test
+	 public void testMax() {
+	 assertNotNull(w.getMax());
+	 assertTrue(w.getMax()>0 && w.getMax() > w.getMin());
+	 }
 }
