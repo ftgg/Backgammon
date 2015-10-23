8621ecca80f32214196c4a9a0093a51f3e78a9a7
ftgg <fabian.gendusa@htwg-konstanz.de>
2015-10-23 09:44:46 +0200

    TeilImplementierung von Dreieck und DreieckTest

--- src/de/htwg/backgammon/Dreieck.java
+++ src/de/htwg/backgammon/Dreieck.java
@@ -1,5 +1,101 @@
 package de.htwg.backgammon;
 
+import java.util.EmptyStackException;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Stack;
 public class Dreieck {
-
+	
+	
+	private Stack<Stein> content;
+	private int size;
+	private int color;
+	private boolean unsecure = true;
+	
+	Dreieck(){
+		content = new Stack<>();
+		size = 0;
+		color = -1;
+	}
+	
+   /*
+	* @return a the stone for bar or null
+	*/
+	public Stein add(Stein s){
+		// Unsicherers Feld und anderer Stein.
+		Stein old;
+		if(unsecure() && s.getColor() != color){
+			old = content.pop();
+			content.push(s);
+			return old;
+		}
+		
+		//Sicher und Fremdes Feld
+		if(!unsecure() && s.getColor() != color)
+			return s;
+		
+		//Sicher und Eigenes Feld
+		content.push(s);
+		size++;
+		if(size >= 2)
+			unsecure = false;
+		return null;
+	}
+	
+	/*
+	 *@return false if two or more stones are on the field 
+	 */
+	public boolean unsecure(){
+		return unsecure;
+	}
+	
+	/*
+	 *@returns a removed stone or null
+	 */ 
+	public Stein remove(){
+		Stein r; 
+		try {
+			r = content.pop();
+		}catch (EmptyStackException e){
+			r = null;
+		}
+		if(r == null){
+			return null;
+		}else{
+			size--;
+			return r;
+		}
+	}
+	/*
+	 * @returns the Amount of stones in Field
+	 */
+	public int count(){
+		return size;
+	}
+	/*
+	 * @returns the color of Stones on the Field 
+	 */
+	public int getColor(){
+		return Stein.Black;
+	}
+	
+	/*
+	 * @returns true if Dreieck is Empty
+	 */
+	public boolean isEmpty(){
+		return false;
+	}
+	
+	/*
+	 * clears the Dreieck
+	 * returns a Set of Stones
+	 */
+	
+	public Set<Stein> clear (){
+		return new TreeSet<Stein>();
+	}
+	
+	
+	
+	
 }
--- test/de/htwg/backgammon/DreieckTest.java
+++ test/de/htwg/backgammon/DreieckTest.java
@@ -7,13 +7,62 @@ import org.junit.Test;
 
 public class DreieckTest {
 
+	Dreieck d;
 	@Before
 	public void setUp() throws Exception {
+		d = new Dreieck();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void add() {
+		Stein s = new Stein(Stein.Black);
+		d.add(s);
+		assertSame(s, d.remove());
 	}
-
+	
+	@Test
+	public void remove(){
+		d.clear();
+		Stein s = new Stein(Stein.White);
+		d.add(s);
+		assertSame (s,d.remove());
+	}
+	
+	@Test
+	public void count(){
+		Stein s = new Stein(Stein.White);
+		int count = d.count();
+		d.add(s);
+		assertSame(count + 1 ,d.count());
+	}
+	
+	@Test
+	public void getColor(){
+		Stein s = new Stein(Stein.White);
+		d.clear();
+		d.add(s);
+		assertSame(Stein.White,d.getColor());
+		
+	}
+	
+	@Test
+	public void clear(){
+		d.add(new Stein(Stein.White));
+		assertNotSame(0,d.clear().size());
+	}
+	
+	@Test
+	public void isEmpty(){
+		d.clear();
+		assertTrue(d.isEmpty());
+	}
+	
+	@Test
+	public void unsecure(){
+		d.clear();
+		d.add(new Stein(Stein.White));
+		d.add(new Stein(Stein.White));
+		assertTrue(d.isEmpty());
+	}
+	
 }
