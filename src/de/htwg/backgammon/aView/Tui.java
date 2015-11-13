b783e9d04daf8a8a91c34ed6d6c74f2bba0c12e2
thgnaedi <thgnaedi@3dlab3.in.fhkn.de>
2015-11-13 12:40:07 +0100

    nothing interesting

--- src/de/htwg/backgammon/aView/Tui.java
+++ src/de/htwg/backgammon/aView/Tui.java
@@ -11,24 +11,24 @@ public class Tui implements Observer {
 
 	private GameState gs;
 	private Controller contr;
-
+	private Scanner sc = new Scanner(System.in);
 	public Tui(Controller c) {
 		contr = c;
 		contr.add(this);
 		initNames();
 		running();
 	}
-
+	
 	private void initNames() {
 		String s1;
 		String s2;
 		System.out.println("Hallo, bitte zwei Spielernamen angeben:");
 		System.out.println("Name Spieler Weiss:");
-		Scanner s = new Scanner(System.in);
-		s1 = s.next();
+		
+		s1 = sc.next();
 		System.out.println("Name Spieler Schwarz:");
-		s2 = s.next();
-		s.close();
+		s2 = sc.next();
+		
 		System.out.printf("Viel Spass %s und %s\n", s1, s2);
 		contr.setSpieler(s1, s2);
 	}
@@ -36,18 +36,16 @@ public class Tui implements Observer {
 	private void running() {
 		while (!gs.getGameFinished()) {
 			System.out.println(gs.getMessage());
-			print();
+			//print();
 			eingabe();
 		}
 	}
 
 	private void eingabe(){
 		System.out.println(gs.getCurrent().getName() + " ist am Zug:");
-		Scanner s = new Scanner(System.in);
-		//TODO s.nextLine genau so wie s.next() wirft NoSuchElementExc
-		String t = s.nextLine();
-		s.close();
-		contr.doAction(t);
+		String input;
+		input = sc.nextLine();
+		contr.doAction(input);
 	}
 	
 	public void print() {
