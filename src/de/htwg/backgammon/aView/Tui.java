ec9eb323e496293b0ba1cf877e4e0415031cb322
thgnaedi <thgnaedi@3dlab2.in.fhkn.de>
2015-11-19 18:18:54 +0100

    removed not working methods

--- src/de/htwg/backgammon/aView/Tui.java
+++ src/de/htwg/backgammon/aView/Tui.java
@@ -36,7 +36,7 @@ public class Tui implements Observer {
 	private void running() {
 		while (!gs.getGameFinished()) {
 			System.out.println(gs.getMessage());
-			//print();
+			printField();
 			eingabe();
 		}
 	}
@@ -48,40 +48,8 @@ public class Tui implements Observer {
 		contr.doAction(input);
 	}
 	
-	public void print() {
-		int feldbreite = gs.getBlackStones().length; //anzahl felder!
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= feldbreite/2 ; i++){
-			addWhitespace(i,sb);
-		}
-		System.out.println(sb.toString());
-		printTokens(1,feldbreite/2);
-	}
-
-	//TODO gibt noch seltsamme sachen aus
-	private void printTokens(int start, int end) {
-		//gibt w für weisse und s für schwarze steine aus
-		int[] bs = gs.getBlackStones();
-		int[] ws = gs.getWhiteStones();
-		StringBuilder sb = new StringBuilder();
-		while(start <= end){
-			if(bs[start] != 0){
-				sb.append(bs[start]).append("S");
-			}
-			else if(ws[start] != 0){
-				sb.append(ws[start]).append("W");
-			}else {
-				sb.append("  ");
-			}
-			start++;
-		}
-		System.out.println(sb.toString());
-	}
-
-	private void addWhitespace(int a,StringBuilder sb){
-		sb.append(a).append(" ");
-		if(a < 10)
-			sb.append(" ");
+	private void printField(){
+		
 	}
 	
 	@Override
