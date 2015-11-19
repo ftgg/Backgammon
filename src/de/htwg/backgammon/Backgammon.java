4c17b4e43e9343f9b55dc57ec4a80dfa502cded1
thgnaedi <thgnaedi@3dlab2.in.fhkn.de>
2015-11-19 17:50:25 +0100

    removed TODO autogen method

--- src/de/htwg/backgammon/Backgammon.java
+++ src/de/htwg/backgammon/Backgammon.java
@@ -6,14 +6,11 @@ import de.htwg.backgammon.controller.Controller;
 public class Backgammon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// TODO schichten aufbauen (controller,model usw..)
 		
 		Controller c = new Controller();
 		Tui tui = new Tui(c);
 		
-		// while(continue){TUI welche dann auch continue auf false setzt,
-		// solange endlosschleife}
 	}
 
 }
