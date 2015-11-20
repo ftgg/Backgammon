bd8d880d751d571279526f20b042001188ac0bf1
thgnaedi <thgnaedi@3dlab3.in.fhkn.de>
2015-11-20 13:31:40 +0100

    no longer working =C

--- src/de/htwg/backgammon/aView/Tui.java
+++ src/de/htwg/backgammon/aView/Tui.java
@@ -8,32 +8,33 @@ import de.htwg.backgammon.util.Event;
 import de.htwg.backgammon.util.Observer;
 
 public class Tui implements Observer {
-	//Alle Feldzahlen eingabe  1-24 intern 0 -23
+	// Alle Feldzahlen eingabe 1-24 intern 0 -23
 	private GameState gs;
 	private Controller contr;
 	private Scanner sc = new Scanner(System.in);
-	
+
 	public Tui(Controller c) {
 		contr = c;
 		contr.add(this);
 		initNames();
 		running();
 	}
-	//nur für Test
-	public Tui(){
-		//gs = new GameState(null, null, null)// hollymollymo
+
+	// nur für Test
+	public Tui() {
+		// gs = new GameState(null, null, null)// hollymollymo
 	}
-	
+
 	private void initNames() {
 		String s1;
 		String s2;
 		System.out.println("Hallo, bitte zwei Spielernamen angeben:");
 		System.out.println("Name Spieler Weiss:");
-		
+
 		s1 = sc.next();
 		System.out.println("Name Spieler Schwarz:");
 		s2 = sc.next();
-		
+
 		System.out.printf("Viel Spass %s und %s\n", s1, s2);
 		contr.setSpieler(s1, s2);
 	}
@@ -46,22 +47,23 @@ public class Tui implements Observer {
 		}
 	}
 
-	private void eingabe(){
+	private void eingabe() {
 		System.out.println(gs.getCurrent().getName() + " ist am Zug:");
 		String input;
 		input = sc.nextLine();
 		contr.doAction(input);
 	}
-	
-	public void printField(){
-		printNumbers(gs.getWhiteStones().length / 2 ,true);
-		printNumbers(gs.getWhiteStones().length,false);
+
+	public void printField() {
+		printNumbers(gs.getWhiteStones().length / 2, false);
+		// TODO Spielfeld ausgeben
+		printNumbers(gs.getWhiteStones().length, true);
 	}
-	
-	private  void printNumbers(int a, boolean left) {
+
+	private void printNumbers(int a, boolean left) {
 		StringBuilder sb = new StringBuilder();
 		if (left) {
-			for (int i = a/2 + 1; i <= a; i++) {
+			for (int i = a / 2 + 1; i <= a; i++) {
 				sb.append(whitespace(i));
 			}
 		} else {
@@ -77,7 +79,7 @@ public class Tui implements Observer {
 			return "  " + i;
 		return " " + i;
 	}
-	
+
 	@Override
 	public void update(Event e) {
 		if (e instanceof GameState) {
