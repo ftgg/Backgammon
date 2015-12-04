b095d8ffb94e38a8736edc59707aeaec8cfe894f
thgnaedi <thgnaedi@3dlab3.in.fhkn.de>
2015-12-04 13:37:47 +0100

    Interface Dice

---
+++ src/de/htwg/backgammon/model/Dice.java
@@ -0,0 +1,40 @@
+package de.htwg.backgammon.model;
+
+public interface Dice {
+
+	/**
+	 * checks if both dice numbers are the same
+	 * 
+	 * @return true if both dice numbers are the same
+	 */
+	public boolean isDoublets();
+
+	/**
+	 * get an int array wich contains all diced cube numbers
+	 * 
+	 * @return current cube numbers
+	 */
+	public int[] getCurrentCubeNumbers();
+
+	/**
+	 * rolls the dices
+	 * 
+	 * @return int array with dice result
+	 */
+	public int[] rollTheDice();
+
+	/**
+	 * get maximum value of dice
+	 * 
+	 * @return max dice value
+	 */
+	public int getMax();
+
+	/**
+	 * get minimum value of dice
+	 * 
+	 * @return min dice value
+	 */
+	public int getMin();
+
+}
