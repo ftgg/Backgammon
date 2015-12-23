650d4f13f9d4b2926750d42f4b5aea1ea336b5ce
thgnaedi <thgnaedi@3dlab3.in.fhkn.de>
2015-12-23 12:04:53 +0100

    tui and gui parallel =)

--- src/de/htwg/backgammon/Backgammon.java
+++ src/de/htwg/backgammon/Backgammon.java
@@ -1,23 +1,33 @@
 package de.htwg.backgammon;
 
+import java.util.Scanner;
+
 import de.htwg.backgammon.aview.Tui;
 import de.htwg.backgammon.aview.gui.Gui;
 import de.htwg.backgammon.controller.Controller;
 
 public class Backgammon {
 
+	private static Scanner sc = new Scanner(System.in);
+	private static Gui gui;
+	private static Tui tui;
+	private static Controller c;
+	
+	
 	private Backgammon() {
 	}
 
 	public static void main(String[] args) {
-		Controller c = new Controller();
-		new Tui(c);
-		new Gui(c);
-//		new Test();
-		new Tui(c);
-		// new Gui(c);
-		// new Test();
+		c = new Controller();
+		tui =new Tui(c);
+		gui =new Gui(c);
 		c.create();
+		
+		boolean continu = true;
+		while(continu){
+			continu = tui.processInputLine(sc.nextLine());
+		}
+		
 	}
 
 }
