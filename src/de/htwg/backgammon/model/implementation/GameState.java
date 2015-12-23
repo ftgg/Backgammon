57eede7c390cf5b6cfc6af42c2221ed1e4da4f8f
ftgg <fabian.gendusa@htwg-konstanz.de>
2015-12-23 09:37:33 +0100

    TestGamestate4

--- src/de/htwg/backgammon/model/implementation/GameState.java
+++ src/de/htwg/backgammon/model/implementation/GameState.java
@@ -128,6 +128,9 @@ public class GameState implements Event,Serializable {
 			return new GameState(new int[] { 1,0,0,0 },new int[]{ 0,0,0,1 },1,0);
 		}else if (i == 3){ // mit stein auf Bar
 			return new GameState(new int[] { 1,0,0,0 },new int[]{ 0,0,0,1 },1,1);
+		}else if (i == 4){
+			return new GameState(new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,3,2,1,3},
+					new int[]{4,3,2,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},0,0);
 		}
 		return getDefaultGameState();
 	}
