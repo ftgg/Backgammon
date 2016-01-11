b5b9661e6c59e55b77d88038f8a9fe114e72803a
ftgg <fabian.gendusa@htwg-konstanz.de>
2016-01-11 17:10:42 +0100

    removed some comments

--- src/de/htwg/backgammon/controller/ExitMoveVerifier.java
+++ src/de/htwg/backgammon/controller/ExitMoveVerifier.java
@@ -7,12 +7,11 @@ public class ExitMoveVerifier extends MoveVerifier {
 
 	@Override
 	public boolean checkMove(int a, int b, int[] zuege, IPitch sf, IPlayer s, IPlayer s1, IPlayer s2) {
-//		System.out.println("Exit Verifier: "+ isExitMoveValid(b, sf, s));
 		return isExitMoveValid(b, sf, s) && successor.checkMove(a, b, zuege, sf, s, s1, s2);
 	}
 
 	public boolean isExitMoveValid(int b, IPitch sf, IPlayer c) {
-		return !(b == IPitch.EXIT) || sf.allHome(c); // Implikation
+		return !(b == IPitch.EXIT) || sf.allHome(c);
 	}
 
 }
